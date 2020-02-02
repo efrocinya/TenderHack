694e0fd21494ff8610388cbedaf8771d92f204ce
efrocinya <58384097+efrocinya@users.noreply.github.com>
2020-02-02 17:32:24 +0300

    Add files via upload

---
+++ ML.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Trainers.FastTree;
+using System.IO;
+namespace BalticSeaHack
+{
+   
+}
---
+++ Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BalticSeaHack
+{
+    public class Product
+    {
+        
+        public string name { get; set; }
+        public int id { get; set; }
+        public int idImage { get; set; }
+        public string kind { get; set; }
+        public float startPrice { get; set; }
+    }
+}
